d801dc5b68b904c4af06a89746a8c12127b4730b
NohDongWan <ndw8022@gmail.com>
2022-11-01 18:55:15 +0900

    init

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MembeRepository {
+    // 일단은 저장소가 선정되지 않은 상태라 store가 디비라고 생각하면 된다.
+    public static Map<Long, Member> store = new HashMap<>();
+    public static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findALl() {
+        return new ArrayList<>(store.values());
+    }
+}
